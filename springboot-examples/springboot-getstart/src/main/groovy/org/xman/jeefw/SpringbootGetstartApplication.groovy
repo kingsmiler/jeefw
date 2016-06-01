fbda99c5ec06c445a780305d3e6af761c5b0e956
kingsmiler <kingsmiler@163.com>
2016-06-01 09:27:43 +0800

    add CompileStatic

--- springboot-examples/springboot-getstart/src/main/groovy/org/xman/jeefw/SpringbootGetstartApplication.groovy
+++ springboot-examples/springboot-getstart/src/main/groovy/org/xman/jeefw/SpringbootGetstartApplication.groovy
@@ -1,9 +1,11 @@
 package org.xman.jeefw
 
+import groovy.transform.CompileStatic
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration
 
+@CompileStatic
 @SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 class SpringbootGetstartApplication {
 
